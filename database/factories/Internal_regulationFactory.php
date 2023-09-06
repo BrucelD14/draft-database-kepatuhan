91bf4ab6e7e55e3ca8da8b7632f443adf40331d2
BrucelD14 <samuderaduta144@gmail.com>
2023-09-06 07:43:46 +0700

    add internal regulation factory, fix internal regulation table

---
+++ database/factories/Internal_regulationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Internal_regulation>
+ */
+class Internal_regulationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nomor_peraturan' => fake()->bothify('???-###'),
+            'tentang' => fake()->paragraph(),
+            'jenis_peraturan' => 'Peraturan Direksi',
+            'keterangan' => fake()->paragraph()
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,52 +23,54 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Internal_regulation::create([
-            'nomor_peraturan' => 'PI-E-1',
-            'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'jenis_peraturan' => 'Peraturan Direksi',
-            'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'dokumen' => 'pie1.pdf'
-        ]);
+        // Internal_regulation::create([
+        //     'nomor_peraturan' => 'PI-E-1',
+        //     'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'jenis_peraturan' => 'Peraturan Direksi',
+        //     'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'dokumen' => 'pie1.pdf'
+        // ]);
 
-        Internal_regulation::create([
-            'nomor_peraturan' => 'PI-E-2',
-            'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'jenis_peraturan' => 'Peraturan Direksi',
-            'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'dokumen' => 'pie2.pdf'
-        ]);
+        // Internal_regulation::create([
+        //     'nomor_peraturan' => 'PI-E-2',
+        //     'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'jenis_peraturan' => 'Peraturan Direksi',
+        //     'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'dokumen' => 'pie2.pdf'
+        // ]);
 
-        External_regulation::create([
-            'nomor_peraturan' => 'PE-E-1',
-            'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'jenis_peraturan' => 'Undang-Undang',
-            'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'dokumen' => 'pee1.pdf'
-        ]);
+        // External_regulation::create([
+        //     'nomor_peraturan' => 'PE-E-1',
+        //     'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'jenis_peraturan' => 'Undang-Undang',
+        //     'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'dokumen' => 'pee1.pdf'
+        // ]);
 
-        External_regulation::create([
-            'nomor_peraturan' => 'PE-E-2',
-            'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'jenis_peraturan' => 'Undang-Undang',
-            'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'dokumen' => 'pee2.pdf'
-        ]);
+        // External_regulation::create([
+        //     'nomor_peraturan' => 'PE-E-2',
+        //     'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'jenis_peraturan' => 'Undang-Undang',
+        //     'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'dokumen' => 'pee2.pdf'
+        // ]);
 
-        Ministerial_regulation::create([
-            'nomor_peraturan' => 'PM-E-1',
-            'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'jenis_peraturan' => 'Peraturan Menteri',
-            'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'dokumen' => 'pme1.pdf'
-        ]);
+        // Ministerial_regulation::create([
+        //     'nomor_peraturan' => 'PM-E-1',
+        //     'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'jenis_peraturan' => 'Peraturan Menteri',
+        //     'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'dokumen' => 'pme1.pdf'
+        // ]);
 
-        Ministerial_regulation::create([
-            'nomor_peraturan' => 'PM-E-2',
-            'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'jenis_peraturan' => 'Peraturan Menteri',
-            'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
-            'dokumen' => 'pme2.pdf'
-        ]);
+        // Ministerial_regulation::create([
+        //     'nomor_peraturan' => 'PM-E-2',
+        //     'tentang' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'jenis_peraturan' => 'Peraturan Menteri',
+        //     'keterangan' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eos nisi vel, deserunt voluptatum quae iste neque?</p>',
+        //     'dokumen' => 'pme2.pdf'
+        // ]);
+
+        Internal_regulation::factory(20)->create();
     }
 }
