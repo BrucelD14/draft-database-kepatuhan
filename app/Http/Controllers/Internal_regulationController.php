7ab6896777cb2358673642076d9ccc230fd2e9fe
BrucelD14 <samuderaduta144@gmail.com>
2023-09-06 14:39:38 +0700

    fixing search

--- app/Http/Controllers/Internal_regulationController.php
+++ app/Http/Controllers/Internal_regulationController.php
@@ -9,18 +9,12 @@ class Internal_regulationController extends Controller
 {
     public function index()
     {
-        $internal_regulations = Internal_regulation::latest();
 
-        if (request('search')) {
-            $internal_regulations->where('tentang', 'like', '%' . request('search') . '%')
-                ->orWhere('nomor_peraturan', 'like', '%' . request('search') . '%')
-                ->orWhere('jenis_peraturan', 'like', '%' . request('search') . '%');
-        }
 
         return view('regulations', [
             'title' => 'Peraturan Internal',
             // 'reg_list' => Internal_regulation::all(),
-            'reg_list' => $internal_regulations->get()
+            'reg_list' => Internal_regulation::latest()->filter(request('search'))->get()
         ]);
     }
 }
--- app/Models/Internal_regulation.php
+++ app/Models/Internal_regulation.php
@@ -11,4 +11,13 @@ class Internal_regulation extends Model
 
     // protected $fillable = ['nomor_peraturan', 'tanggal_ditetapkan', 'tentang', 'status', 'dokumen', 'published_at'];
     protected $guarded = ['id'];
+
+    public function scopeFilter($query)
+    {
+        if (request('search')) {
+            return $query->where('tentang', 'like', '%' . request('search') . '%')
+                ->orWhere('nomor_peraturan', 'like', '%' . request('search') . '%')
+                ->orWhere('jenis_peraturan', 'like', '%' . request('search') . '%');
+        }
+    }
 }
