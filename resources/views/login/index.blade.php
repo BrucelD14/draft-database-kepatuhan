0841eddfaa9df6db66c795a85823458e1693f05d
BrucelD14 <samuderaduta144@gmail.com>
2023-09-07 11:05:58 +0700

    fix view form login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,8 +2,6 @@
 <html lang="en" data-bs-theme="auto">
 
 <head>
-    <script src="../assets/js/color-modes.js"></script>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -20,32 +18,37 @@
 
 </head>
 
-<body class="d-flex align-items-center py-4 bg-body-tertiary">
+<body>
 
-    <main class="form-signin w-100 m-auto">
-        <form>
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-4">
+                <main class="form-signin text-center">
+                    <h1 class="h3 mb-3 fw-normal">Please login</h1>
+                    <form>
+                        <div class="form-floating">
+                            <input type="email" class="form-control" id="floatingInput"
+                                placeholder="name@example.com">
+                            <label for="floatingInput">NIP</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                        <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
+                    </form>
+                </main>
+            </div>
+        </div>
+    </div>
 
-            <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
-            </div>
 
-            <div class="form-check text-start my-3">
-                <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-                <label class="form-check-label" for="flexCheckDefault">
-                    Remember me
-                </label>
-            </div>
-            <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-            <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
-        </form>
-    </main>
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+    {{-- Boostrap script --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
 </body>
 
