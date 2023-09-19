cd7f8c1f997832e2564526aa616181e5f395d8f0
BrucelD14 <samuderaduta144@gmail.com>
2023-09-19 07:58:23 +0700

    add navbar brand

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -1,10 +1,13 @@
 <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-5 text-white" href="/">JDIH INKA</a>
+    <a class="navbar-brand col-md-3 col-lg-2 px-3 text-center" href="/">
+        <img src="img/inkawb.png" alt="" width="150">
+    </a>
 
     <ul class="navbar-nav flex-row d-md-none">
         <li class="nav-item text-nowrap">
-            <button class="nav-link px-3 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch"
-                aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search">
+            <button class="nav-link px-3 text-white" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSearch" aria-controls="navbarSearch" aria-expanded="false"
+                aria-label="Toggle search">
                 <svg class="bi">
                     <use xlink:href="#search" />
                 </svg>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="row justify-content-center border-bottom p-4">
+    <div class="row justify-content-center border-bottom pt-4 pb-1">
         <div class="col-md-12 text-center p-2">
             <h3 class="fw-bold">SELAMAT DATANG DI JARINGAN DOKUMENTASI DAN INFORMASI HUKUM (JDIH)</h3>
             <h1 class="fw-bold">PT INDUSTRI KERETA API (Persero)</h1>
