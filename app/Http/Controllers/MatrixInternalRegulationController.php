2b715e2538139cd332bfb6dedd47ff0a6bd38cd4
BrucelD14 <samuderaduta144@gmail.com>
2023-10-03 13:49:23 +0700

    custom chart

--- app/Http/Controllers/MatrixInternalRegulationController.php
+++ app/Http/Controllers/MatrixInternalRegulationController.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Charts\PeraturanInternalChart;
+use App\Models\Internal_regulation;
 
 class MatrixInternalRegulationController extends Controller
 {
     public function index(PeraturanInternalChart $chart)
     {
+        $tahun = date('Y');
+        $periode = $tahun - 4;
+        for ($i = $periode; $i <= $tahun; $i++) {
+            $totalPeraturanDireksi = Internal_regulation::whereYear('tanggal_penetapan', $i)->where('jenis_peraturan_internal_id', 1)->count();
+            $totalSuratEdaran = Internal_regulation::whereYear('tanggal_penetapan', $i)->where('jenis_peraturan_internal_id', 2)->count();
+            $dataTahun[] = $i;
+            $dataTotalPeraturanDireksi[] = $totalPeraturanDireksi;
+            $dataTotalSuratEdaran[] = $totalSuratEdaran;
+        };
         return view('matriks.peraturanInternal.index', [
             'title' => 'Matriks Peraturan Internal',
             'chart' => $chart->build(),
+            'periode' => $dataTahun,
+            'peraturanDireksi' => $dataTotalPeraturanDireksi,
+            'suratEdaran' => $dataTotalSuratEdaran,
         ]);
     }
 }
--- resources/views/matriks/peraturanInternal/index.blade.php
+++ resources/views/matriks/peraturanInternal/index.blade.php
@@ -9,4 +9,33 @@
     <div class="row py-2">
         {!! $chart->container() !!}
     </div>
+    <div class="row py-3">
+        <table class="table table-bordered">
+            <thead>
+                <tr class="table-secondary text-center">
+                    <th scope="col">#</th>
+                    <th scope="col">Jenis Peraturan</th>
+                    @foreach ($periode as $p)
+                        <th scope="col">{{ $p }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="text-center">
+                    <th scope="row">1</th>
+                    <td>Peraturan Direksi</td>
+                    @foreach ($peraturanDireksi as $item)
+                        <td>{{ $item }}</td>
+                    @endforeach
+                </tr>
+                <tr class="text-center">
+                    <th scope="row">2</th>
+                    <td>Surat Edaran</td>
+                    @foreach ($suratEdaran as $item)
+                        <td>{{ $item }}</td>
+                    @endforeach
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
