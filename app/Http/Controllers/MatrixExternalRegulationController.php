24f22647eb7b55839816246178e5dd437e3d5d95
BrucelD14 <samuderaduta144@gmail.com>
2023-10-17 10:52:08 +0700

    add filter year and reset button for Matriks Peraturan Internal & Matriks Reviu Peraturan Eksternal

--- app/Http/Controllers/MatrixExternalRegulationController.php
+++ app/Http/Controllers/MatrixExternalRegulationController.php
@@ -8,9 +8,15 @@ use Illuminate\Http\Request;
 
 class MatrixExternalRegulationController extends Controller
 {
-    public function index(PeraturanEksternalChart $chart)
+    public function index(PeraturanEksternalChart $chart, Request $request)
     {
-        $tahun = date('Y');
+        // $tahun = date('Y');
+        $year = $request->query('tahun');
+        if (empty($year)) {
+            $tahun = date('Y');
+        } else {
+            $tahun = $year;
+        }
         $periode = $tahun - 4;
         for ($i = $periode; $i <= $tahun; $i++) {
             $totalUndangUndang = ReviewEksternalReg::whereYear('tanggal_penetapan', $i)->where('jenis_peraturan_eksternal_id', 1)->count();
@@ -21,10 +27,11 @@ class MatrixExternalRegulationController extends Controller
         };
         return view('matriks.peraturanEksternal.index', [
             'title' => 'Matriks Reviu Peraturan Eksternal',
-            'chart' => $chart->build(),
+            'chart' => $chart->build($tahun),
             'periode' => $dataTahun,
             'undangUndang' => $dataTotalUndangUndang,
             'peraturanPemerintah' => $dataTotalPeraturanPemerintah,
+            'tahun' => $tahun
         ]);
     }
 }
--- app/Http/Controllers/MatrixInternalRegulationController.php
+++ app/Http/Controllers/MatrixInternalRegulationController.php
@@ -8,9 +8,15 @@ use App\Models\Internal_regulation;
 
 class MatrixInternalRegulationController extends Controller
 {
-    public function index(PeraturanInternalChart $chart)
+    public function index(PeraturanInternalChart $chart, Request $request)
     {
-        $tahun = date('Y');
+        // $tahun = date('Y');
+        $year = $request->query('tahun');
+        if (empty($year)) {
+            $tahun = date('Y');
+        } else {
+            $tahun = $year;
+        }
         $periode = $tahun - 4;
         for ($i = $periode; $i <= $tahun; $i++) {
             $totalPeraturanDireksi = Internal_regulation::whereYear('tanggal_penetapan', $i)->where('jenis_peraturan_internal_id', 1)->count();
@@ -21,11 +27,12 @@ class MatrixInternalRegulationController extends Controller
         };
         return view('matriks.peraturanInternal.index', [
             'title' => 'Matriks Peraturan Internal',
-            'chart' => $chart->build(),
+            'chart' => $chart->build($tahun),
             'periode' => $dataTahun,
             'peraturanDireksi' => $dataTotalPeraturanDireksi,
             'suratEdaran' => $dataTotalSuratEdaran,
             'peraturan' => Internal_regulation::all(),
+            'tahun' => $tahun
         ]);
     }
 }
