862822138c3b6beb141709c658be6cea898d9d00
BrucelD14 <samuderaduta144@gmail.com>
2023-11-02 11:27:56 +0700

    fix link navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -60,15 +60,15 @@ https://templatemo.com/tm-590-topic-listing
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-lg-5 me-lg-auto">
                         <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_1">Beranda</a>
+                            <a class="nav-link click-scroll" href="/landing">Beranda</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_2">Produk Hukum</a>
+                            <a class="nav-link click-scroll" href="/produk_hukum">Produk Hukum</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_3">Matriks</a>
+                            <a class="nav-link click-scroll" href="/matriks">Matriks</a>
                         </li>
 
                         <li class="nav-item dropdown">
@@ -76,10 +76,19 @@ https://templatemo.com/tm-590-topic-listing
                                 role="button" data-bs-toggle="dropdown" aria-expanded="false">Extra</a>
 
                             <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                                <li><a class="dropdown-item" href="contact.html"><i
+                                <li><a class="dropdown-item" href="/dashboard"><i
                                             class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
-                                <li><a class="dropdown-item" href="contact.html"><i class="bi bi-box-arrow-right"></i>
-                                        Logout</a></li>
+                                <li>
+                                    {{-- <a class="dropdown-item" href="contact.html"><i class="bi bi-box-arrow-right"></i>
+                                        Logout</a> --}}
+                                    <form action="/logout" method="post">
+                                        @csrf
+                                        <button type="submit" class="dropdown-item"
+                                            onclick="return confirm('Yakin mau Logout?')"><i
+                                                class="bi bi-box-arrow-right"></i>
+                                            Logout</button>
+                                    </form>
+                                </li>
                             </ul>
                         </li>
                     </ul>
@@ -337,15 +346,15 @@ https://templatemo.com/tm-590-topic-listing
 
                     <ul class="site-footer-links">
                         <li class="site-footer-link-item">
-                            <a href="#" class="site-footer-link">Beranda</a>
+                            <a href="/landing" class="site-footer-link">Beranda</a>
                         </li>
 
                         <li class="site-footer-link-item">
-                            <a href="#" class="site-footer-link">Produk Hukum</a>
+                            <a href="/produk_hukum" class="site-footer-link">Produk Hukum</a>
                         </li>
 
                         <li class="site-footer-link-item">
-                            <a href="#" class="site-footer-link">Matriks</a>
+                            <a href="/matriks" class="site-footer-link">Matriks</a>
                         </li>
                     </ul>
                 </div>
