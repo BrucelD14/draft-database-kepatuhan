1cd7e05ab1bb677c727cf8dfbb0adcdf31e49271
BrucelD14 <samuderaduta144@gmail.com>
2023-11-03 10:14:38 +0700

    rebuild UI Login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,74 +1,130 @@
-<!doctype html>
-<html lang="en" data-bs-theme="auto">
+<!DOCTYPE html>
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default"
+    data-assets-path="../assets/" data-template="vertical-menu-template-free">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.112.5">
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
     <title>JDIH | {{ $title }}</title>
 
-    {{-- Bootstrap CSS --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta name="description" content="" />
 
-    {{-- My CSS --}}
-    <link rel="stylesheet" href="/css/style.css">
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
 
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="css/login/boxicons.css" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="css/login/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="css/login/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="css/login/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="css/login/perfect-scrollbar.css" />
+
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="css/login/page-auth.css" />
+    <!-- Helpers -->
+    <script src="js/login/helpers.js"></script>
+    <script src="js/login/config.js"></script>
 </head>
 
 <body>
+    <!-- Content -->
 
-    <div class="container">
-        <div class="hero text-center mt-4 p-2 border border-start-0 border-end-0 border-2">
-            <h4 class="fw-bold">JARINGAN DOKUMENTASI DAN INFORMASI HUKUM (JDIH)</h4>
-            <h2 class="fw-bold">PT INDUSTRI KERETA API (Persero)</h2>
-        </div>
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-4">
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="index.html" class="app-brand-link">
+                                <img src="{{ asset('img/inka.png') }}" alt="PT INKA (Persero)" width="230">
+                            </a>
+                        </div>
+                        <!-- /Logo -->
+                        <h5 class="mb-2">Welcome to JDIH PT INKA (Persero)! 👋</h5>
+                        <p class="mb-4">Please login to your account and start the adventure</p>
 
-                @if (session()->has('loginError'))
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ session('loginError') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
+                        {{-- ERROR ALERT --}}
+                        @if (session()->has('loginError'))
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                {{ session('loginError') }}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                    aria-label="Close"></button>
+                            </div>
+                        @endif
+                        {{-- ERROR ALERT --}}
 
-                <main class="form-signin text-center">
-                    <h1 class="h3 mb-3 fw-normal">Please login</h1>
-                    <form action="/" method="post">
-                        @csrf
-                        <div class="form-floating">
-                            <input type="number" name="nip" class="form-control @error('nip') is-invalid @enderror"
-                                id="nip" placeholder="nip" autofocus required value="{{ old('nip') }}">
-                            <label for="nip">NIP</label>
-                            @error('nip')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
+                        <form id="formAuthentication" class="mb-3" action="/" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="nip" class="form-label">NIP</label>
+                                <input type="text" class="form-control" id="email" name="nip"
+                                    placeholder="Enter your nip" autofocus required value="{{ old('nip') }}" />
+                                @error('nip')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <div class="d-flex justify-content-between">
+                                    <label class="form-label" for="password">Password</label>
                                 </div>
-                            @enderror
-                        </div>
-                        <div class="form-floating">
-                            <input type="password" name="password" class="form-control" id="password"
-                                placeholder="Password" required>
-                            <label for="password">Password</label>
-                        </div>
-                        <button class="btn btn-danger w-100 py-2" type="submit">Login</button>
-                    </form>
-                </main>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" required />
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-primary d-grid w-100" type="submit"
+                                    style="background-color: #80d0c7;border-color:#80d0c7;">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- /Register -->
             </div>
         </div>
     </div>
 
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="js/login/jquery.js"></script>
+    <script src="js/login/popper.js"></script>
+    <script src="js/login/bootstrap.js"></script>
+    <script src="js/login/perfect-scrollbar.js"></script>
 
+    <script src="js/login/menu.js"></script>
+    <!-- endbuild -->
 
+    <!-- Vendors JS -->
 
-    {{-- Boostrap script --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
+    <!-- Main JS -->
+    <script src="js/login/main.js"></script>
 
+    <!-- Page JS -->
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
 </body>
 
 </html>
---
+++ resources/views/login/index_.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.112.5">
+    <title>JDIH | {{ $title }}</title>
+
+    {{-- Bootstrap CSS --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    {{-- My CSS --}}
+    <link rel="stylesheet" href="/css/style.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="hero text-center mt-4 p-2 border border-start-0 border-end-0 border-2">
+            <h4 class="fw-bold">JARINGAN DOKUMENTASI DAN INFORMASI HUKUM (JDIH)</h4>
+            <h2 class="fw-bold">PT INDUSTRI KERETA API (Persero)</h2>
+        </div>
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-4">
+
+                @if (session()->has('loginError'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ session('loginError') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+
+                <main class="form-signin text-center">
+                    <h1 class="h3 mb-3 fw-normal">Please login</h1>
+                    <form action="/" method="post">
+                        @csrf
+                        <div class="form-floating">
+                            <input type="number" name="nip" class="form-control @error('nip') is-invalid @enderror"
+                                id="nip" placeholder="nip" autofocus required value="{{ old('nip') }}">
+                            <label for="nip">NIP</label>
+                            @error('nip')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="form-floating">
+                            <input type="password" name="password" class="form-control" id="password"
+                                placeholder="Password" required>
+                            <label for="password">Password</label>
+                        </div>
+                        <button class="btn btn-danger w-100 py-2" type="submit">Login</button>
+                    </form>
+                </main>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    {{-- Boostrap script --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
