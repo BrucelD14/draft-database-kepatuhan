c594f540e1c0e8a7b9e1bddc8e3c7769507c83b4
BrucelD14 <samuderaduta144@gmail.com>
2023-11-28 10:38:21 +0700

    add info count at page matriks

--- app/Http/Controllers/MatrixController.php
+++ app/Http/Controllers/MatrixController.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Internal_regulation;
+use App\Models\ReviewEksternalReg;
 use Illuminate\Http\Request;
 
 class MatrixController extends Controller
 {
     public function index()
+
     {
+        $year = date('Y');
+        $internalRegCount = Internal_regulation::whereYear('tanggal_penetapan', $year)->count();
+        $externalRevCount = ReviewEksternalReg::whereYear('tanggal_penetapan', $year)->where('status_publish', 1)->count();
+
         return view('matriks.index', [
             'title' => 'Matriks Peraturan',
+            'internalRegCount' => $internalRegCount,
+            'externalRevCount' => $externalRevCount,
+            'year' => $year,
         ]);
     }
 }
--- resources/views/matriks/index.blade.php
+++ resources/views/matriks/index.blade.php
@@ -27,9 +27,20 @@
                     <div class="custom-block bg-white shadow-lg">
                         <div class="d-flex">
                             <div>
-                                <h5 class="mb-2">Matriks Peraturan Internal PT INKA (Persero)</h5>
+                                <h5 class="mb-5">Matriks Peraturan Internal PT INKA (Persero)</h5>
 
-                                <p class="mb-0">Lorem Ipsum dolor sit amet consectetur</p>
+                                <div class="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60"
+                                        fill="currentColor" class="bi bi-file-bar-graph d-block m-auto" viewBox="0 0 16 16">
+                                        <path
+                                            d="M4.5 12a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5zm3 0a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5zm3 0a.5.5 0 0 1-.5-.5v-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-.5.5z" />
+                                        <path
+                                            d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1" />
+                                    </svg>
+                                    <h6 class="text-center mt-2 mb-1">{{ $internalRegCount }}</h6>
+                                    <p class="text-center">Total Peraturan Internal Tahun {{ $year }}</p>
+                                </div>
+
 
                                 <a href="/matriks/peraturan_internal" class="btn custom-btn mt-2 mt-lg-3">Learn
                                     More</a>
@@ -46,10 +57,19 @@
 
                             <div class="custom-block-overlay-text d-flex">
                                 <div>
-                                    <h5 class="text-white mb-2">Matriks Peraturan Eksternal PT INKA (Persero)</h5>
+                                    <h5 class="text-white mb-5">Matriks Peraturan Eksternal PT INKA (Persero)</h5>
 
-                                    <p class="text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint
-                                        animi necessitatibus aperiam repudiandae nam omnis</p>
+                                    <div class="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60"
+                                            fill="currentColor" class="bi bi-file-bar-graph-fill d-block m-auto"
+                                            viewBox="0 0 16 16">
+                                            <path
+                                                d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2m-2 11.5v-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5m-2.5.5a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5zm-3 0a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5z" />
+                                        </svg>
+                                        <h6 class="text-center mt-2 mb-1">{{ $externalRevCount }}</h6>
+                                        <p class="text-center text-white">Total Reviu Peraturan Eksternal Tahun
+                                            {{ $year }}</p>
+                                    </div>
 
                                     <a href="/matriks/peraturan_eksternal" class="btn custom-btn mt-2 mt-lg-3">Learn
                                         More</a>
