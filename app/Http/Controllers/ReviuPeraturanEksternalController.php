4026ee3ea00d3c44ef0c826d2b75e081858b853c
BrucelD14 <samuderaduta144@gmail.com>
2023-12-02 22:38:30 +0700

    fix filter reviu peratutan eksternal

--- app/Http/Controllers/ReviuPeraturanEksternalController.php
+++ app/Http/Controllers/ReviuPeraturanEksternalController.php
@@ -7,6 +7,7 @@ use App\Models\KategoriDivisi;
 use App\Models\KategoriDivisiReviu;
 use App\Models\ReviewEksternalReg;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ReviuPeraturanEksternalController extends Controller
 {
@@ -21,16 +22,31 @@ class ReviuPeraturanEksternalController extends Controller
 
         $hasilPencarian = ReviewEksternalReg::join('kategori_divisi_reviu', 'kategori_divisi_reviu.uuid_review_eksternal_reg', '=', 'review_eksternal_regs.uuid')
             ->join('kategori_divisis', 'kategori_divisis.id', '=', 'kategori_divisi_reviu.kategori_divisi_id')
-            ->where(function ($query) use ($searchKeyword) {
-                $query->where('review_eksternal_regs.tentang', 'like', '%' . $searchKeyword . '%')
-                    ->orWhere('review_eksternal_regs.nomor_peraturan', 'like', '%' . $searchKeyword . '%')
-                    ->orWhere('review_eksternal_regs.tanggal_penetapan', 'like', '%' . $searchKeyword . '%');
+            ->where('review_eksternal_regs.status_publish', 1)
+            ->when($searchKeyword, function ($query) use ($searchKeyword) {
+                $query->where(function ($innerQuery) use ($searchKeyword) {
+                    $innerQuery->where('review_eksternal_regs.tentang', 'like', '%' . $searchKeyword . '%')
+                        ->orWhere('review_eksternal_regs.nomor_peraturan', 'like', '%' . $searchKeyword . '%')
+                        ->orWhere('review_eksternal_regs.tanggal_penetapan', 'like', '%' . $searchKeyword . '%');
+                });
             })
-            ->where(function ($query) use ($selectOptionValueJenis) {
-                $query->where('kategori_divisis.id', '=', $selectOptionValueJenis)
-                    ->where('review_eksternal_regs.jenis_peraturan_eksternal_id', '=', $selectOptionValueJenis);
+            ->when($selectOptionValue, function ($query) use ($selectOptionValue) {
+                $query->where('kategori_divisis.id', '=', $selectOptionValue);
             })
-            ->select('review_eksternal_regs.*')
+            ->when($selectOptionValueJenis, function ($query) use ($selectOptionValueJenis) {
+                $query->where('review_eksternal_regs.jenis_peraturan_eksternal_id', '=', $selectOptionValueJenis);
+            })
+            ->groupBy('review_eksternal_regs.uuid')
+            ->select(
+                'review_eksternal_regs.uuid',
+                DB::raw('MAX(review_eksternal_regs.tanggal_penetapan) as tanggal_penetapan'),
+                DB::raw('MAX(review_eksternal_regs.nomor_peraturan) as nomor_peraturan'),
+                DB::raw('MAX(review_eksternal_regs.tentang) as tentang'),
+                DB::raw('MAX(review_eksternal_regs.status) as status'),
+                DB::raw('MAX(review_eksternal_regs.id) as id'),
+                DB::raw('MAX(review_eksternal_regs.dokumen) as dokumen'),
+            )
+            ->orderBy('id', 'desc')
             ->paginate(5);
 
         return view('reviewEksternalReg.index', [
